7d9af9bebe785a6d61afe983f2c45c6ab70ade6e
ragavims1527 <ragavims.1527@gmail.com>
2026-08-03 11:43:25 +0530

    Add files via upload

---
+++ Homework/HollowSandClock.java
@@ -0,0 +1,27 @@
+public class HollowSandClock {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++)
+                System.out.print(" ");
+            for(int j=0;j<2*(n-i)-1;j++){
+                if(i==0 || j==0 || j==2*(n-i)-2)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<i;j++)
+                System.out.print(" ");
+            for(int j=0;j<2*(n-i)-1;j++){
+                if(i==0 || j==0 || j==2*(n-i)-2)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
